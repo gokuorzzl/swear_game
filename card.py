8e28a73f0acc6243a6bfc710716561f8f80010ca
Kim Myung Sang <gokuorzzl@naver.com>
2015-08-03 19:06:43 +0900

    add swear, compare, result

--- card.py
+++ card.py
@@ -12,3 +12,7 @@ class Card:
 
     def __lt__(self, other):
         return self.num < other.num
+
+    def __repr__(self):
+        return '<Card({})>'.format(self.num)
+    # 리프리젠테이션 해당객체>>문자열로 보여주게끔 동작함
